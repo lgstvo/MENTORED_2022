6d367fa87b87e345a9927757a37b33b169a82d15
Luís Gustavo <lgustavo2001@gmail.com>
2022-06-29 16:09:29 -0300

    CSV generation automation

--- src/save_entropy.py
+++ src/save_entropy.py
@@ -7,19 +7,35 @@ import os
 import argparse
 from dbscan import *
 
-def fetch_package_csv_modified():
-    csv1 =  pd.read_csv("entropy_capture45_1.csv")
-    csv2 =  pd.read_csv("entropy_capture45_2.csv")
-    csv3 =  pd.read_csv("entropy_capture45_3.csv")
-    csv4 =  pd.read_csv("entropy_capture45_4.csv")
-    csv5 =  pd.read_csv("entropy_capture45_5.csv")
-    full_csv =  pd.concat([csv1, csv2, csv3, csv4, csv5], ignore_index=True, axis=0)
-    full_csv = full_csv.drop(labels="Unnamed: 0", axis=1)
-    return full_csv
+def fetch_package_csv_modified(presaved_packets, window_size, dataset):
+    list_csvs = sorted(os.listdir(presaved_packets))
+    for packet_file in list_csvs:
+        print("Processing {}...".format(packet_file))
+        fraction_csv = pd.read_csv(os.path.join(presaved_packets, packet_file))
+        entropy_dframe = entropy_dataframe(fraction_csv, window_size, dataset)
+        entropy_dframe.to_csv("data/capture45/csvs/entropy/entropy_capture45_{}_window{}.csv".format(packet_file.split('.')[0][-1], window_size))
+
+    print("Fetching complete.")
+
+def compact_entropy_csv(presaved_entropies, window_size):
+    list_csvs = sorted(os.listdir(presaved_entropies))
+    dframe = pd.DataFrame(columns=['Source_int', 'Destination_int', "Source_Port", "Destination_Port", "Length", "Infected"])
+    for packet_file in list_csvs:
+        print("Processing {}...".format(packet_file))
+        fraction_csv = pd.read_csv(os.path.join(presaved_entropies, packet_file))
+        dframe =  pd.concat([dframe, fraction_csv], ignore_index=True, axis=0)
+
+    dframe.to_csv(presaved_entropies+"entropy_capture45_window{}.csv".format(window_size))
+    print("Fetching complete.")
 
 def main():
-    dframe = fetch_package_csv_modified()
-    dframe.to_csv("entropy_capture45.csv")
+    presaved_packets = "data/capture45/csvs/packets/"
+    presaved_entropies = "data/capture45/csvs/entropy/"
+    dataset = "ctu13c45"
+    window_size = 5000
+    fetch_package_csv_modified(presaved_packets, window_size, dataset)
+    compact_entropy_csv(presaved_entropies, window_size)
+    print("Completed.")
 
 if __name__ == '__main__':
     main()
